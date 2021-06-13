81f8cf16d6c1d3efc8b7dc9c74868bef6558a41f
fer <ferconde87@gmail.com>
2021-06-13 19:40:56 +0700

    Node JS: News (API Routes, Layout & CSS Modules, Meta, Data Fetching)

--- Next JS/blog/pages/index.js
+++ Next JS/blog/pages/index.js
@@ -1,48 +1,32 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import { server } from '../config'
+import ArticleList from '../components/ArticleList'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+export default function Home({articles}) {
+  return (
+    <div>
+      <ArticleList articles={articles} />
+    </div>
+  )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${server}/api/articles`)
+  const articles = await res.json()
+
   return {
     props: {
-      allPostsData
-    }
+      articles,
+    },
   }
 }
 
-export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p> Hello there! I'm Fer. I'm software engineer and I love to use cutting edge technology to takle hard problems, especially when this envolved handling massive data.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  )
-}
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+//   const articles = await res.json();
+
+//   return {
+//     props: {
+//       articles
+//     }
+//   }
+// }
