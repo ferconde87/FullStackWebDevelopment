90c3020dd530a442586780dfed3f70a20121a8a7
fer <ferconde87@gmail.com>
2021-07-02 17:16:09 +0700

    Ruby Crypto Prices

---
+++ Rails/Ruby-Crypto-Prices/api.rb
@@ -0,0 +1,11 @@
+require './lib/base'
+require './lib/coinbase'
+require './lib/coin_gecko'
+require './lib/coin_paprika'
+require './lib/coin_cap'
+require './lib/crypto_compare'
+
+module Api
+  
+  
+end
