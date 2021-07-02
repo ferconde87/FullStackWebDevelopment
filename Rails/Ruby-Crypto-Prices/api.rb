da6bd9be261e27aaff336cfbedff94d2d73ded35
fer <ferconde87@gmail.com>
2021-07-02 17:43:10 +0700

    Ruby Crypto Prices: Wrapper + methods for each class

--- Rails/Ruby-Crypto-Prices/api.rb
+++ Rails/Ruby-Crypto-Prices/api.rb
@@ -6,6 +6,24 @@ require './lib/coin_cap'
 require './lib/crypto_compare'
 
 module Api
+  class Wrapper < Base
+    %w[
+      coinbase
+      coin_gecko
+      coin_paprika
+      coin_cap
+      crypto_compare
+    ].each do |api|
+      define_method(api.to_sym) do
+        Object.const_get("Api::#{klassify(api)}").new(@options).request
+      end
+    end
+
+    def klassify(name)
+      name.split('_').collect(&:capitalize).join
+    end
+  end
+
   
   
 end
